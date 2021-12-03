86ce26b7604ee90137deb365bb65212037a6f753
Lois Wells <lois.wells@raspberrypi.org>
2021-12-03 20:00:12 +0000

    Solution to Day2 Part 2

---
+++ day2_2.rb
@@ -0,0 +1,19 @@
+file = File.open('day2.txt')
+
+input = file.readlines.map(&:chomp)
+
+final_coords=input.inject([0,0,0]) do |coords, move|
+  move_instructions=move.split
+  if move_instructions[0]=='forward'
+    coords[1]+=move_instructions[1].to_i
+    coords[0]+=move_instructions[1].to_i*coords[2]
+  elsif move_instructions[0]=='down'
+    coords[2]+=move_instructions[1].to_i
+  else
+    coords[2]-=move_instructions[1].to_i
+  end
+  puts(coords)
+  coords
+end
+
+puts(final_coords[0]*final_coords[1])
